e36c31aaab65f173c732da391722d87dffb8018f
Kiuber <kiuber.zhang@gmail.com>
2018-06-24 10:58:09 +0800

    adjust

--- base.sh
+++ base.sh
@@ -7,13 +7,19 @@ function _run_cmd() {
 function _remove_container() {
     local container_name=$1
     local cmd="docker rm -f $container_name"
-    run_cmd "$cmd"
+    _run_cmd "$cmd"
 }
 
 function _send_cmd_to_container() {
     local _container=$1
     local _cmd=$2
     local cmd="docker exec -it $_container $_cmd"
-    run_cmd "$cmd"
+    _run_cmd "$cmd"
 }
 
+function link_node_modules() {
+    if [ -d "$1/node_modules" ]; then
+        _run_cmd "rm $1/node_modules"
+    fi
+    _run_cmd "ln -sf /opt/node_npm_data/node_modules $1"
+}
